6cbe7627392bfb17a1a14b66c6ec333287a309be
Matthias Pigulla <mp@webfactory.de>
2026-03-17 10:59:49 +0100

    Disable JIT when running Psalm to work around https://github.com/vimeo/psalm/issues/11723
    Once the bug is fixed in Psalm and/or in PHP (https://github.com/php/php-src/issues/21460), please open a request to revert this change.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 set -e
 
-/composer/vendor/bin/psalm --threads=$(nproc) "$@"
+echo -- Disabling JIT for Psalm as a workaround for https://github.com/vimeo/psalm/issues/11723
+echo -- You can ignore the JIT-related error message below
+echo
+
+PSALM_ALLOW_XDEBUG=1 /composer/vendor/bin/psalm --threads=$(nproc) "$@"
