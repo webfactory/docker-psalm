a8ef9931eb756e6a6553fdf877872431597ca3da
Matthias Pigulla <mp@webfactory.de>
2026-04-14 10:07:56 +0200

    Revert "Disable JIT when running Psalm to work around https://github.com/vimeo/psalm/issues/11723"
    This reverts commit 6cbe7627392bfb17a1a14b66c6ec333287a309be, since the issue has been fixed in Psalm 6.16.1.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -l
 set -e
 
-echo -- Disabling JIT for Psalm as a workaround for https://github.com/vimeo/psalm/issues/11723
-echo -- You can ignore the JIT-related error message below
-echo
-
-PSALM_ALLOW_XDEBUG=1 /composer/vendor/bin/psalm --threads=$(nproc) "$@"
+/composer/vendor/bin/psalm --threads=$(nproc) "$@"
